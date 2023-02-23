7f92539e2b976a22a0f8ff36091f8909acedcb07
Pedro Varela <93870597+pedro-varela1@users.noreply.github.com>
2023-02-23 20:32:41 -0300

    Update time_calculator.py
    Pedro Varela's version

--- time_calculator.py
+++ time_calculator.py
@@ -1,7 +1,54 @@
-def add_time(start, duration):
+def add_time(start, duration, *d):
+        start = start.split()
+        start[0] = start[0].split(':')
+        duration = duration.split()
+        duration[0] = duration[0].split(':')
 
+        if start[1] == 'PM':
+                start[0][0] = str(int(start[0][0])+12)
 
+        day_number = int((int(duration[0][0]) + int(start[0][0]) + int((int(duration[0][1]) + int(start[0][1]))/60))/24)
+        if day_number == 1:
+                day = '(next day)'
+        elif day_number == 0:
+                day = ''
+        else:
+                day = '(' + str(day_number) + ' days later)'
+        hour = int((int(duration[0][0]) + int(start[0][0]) + int((int(duration[0][1]) + int(start[0][1]))/60)) % 24)
+        if hour >= 13:
+                hour = str(hour - 12)
+                complement = 'PM'
+        elif hour == 12:
+                hour = str(hour)
+                complement = 'PM'
+        else:
+                if hour==0:
+                        hour = 12
+                hour = str(hour)
+                complement = 'AM'
+        min = str(int((int(duration[0][1]) + int(start[0][1])) % 60)).zfill(2)
 
+        result = hour + ':' + min + ' ' + complement + ' ' + day
 
+        #
+        week = dict()
+        week[1] = 'monday'
+        week[2] = 'tuesday'
+        week[3] = 'wednesday'
+        week[4] = 'thursday'
+        week[5] = 'friday'
+        week[6] = 'saturday'
+        week[7] = 'sunday'
 
-    return new_time
+        day_index = 0
+        for dia in d:
+                if type(dia) == str:
+                        for i,day_word in week.items():
+                                if day_word == dia.lower():
+                                        day_index = i
+                                        break
+                        day_index = int((day_index+day_number) % 7)
+                        if day_index == 0:
+                                day_index = 7
+                        result = hour + ':' + min + ' ' + complement + ', ' + week[day_index].capitalize() + ' ' + day
+        return result.rstrip()
